bd7f270af17dda39bbd7bceb5cf08a3932f0ec2e
lidu-1 <lilijoli65@gmail.com>
2022-08-07 12:34:41 +0300

    initial commit

--- models/base_model.py
+++ models/base_model.py
@@ -1,45 +1,52 @@
 #!/usr/bin/python3
-'''Base Model Module'''
-
-from datetime import datetime
-from uuid import uuid4
+"""Defines the BaseModel class."""
 import models
+from uuid import uuid4
+from datetime import datetime
 
 
 class BaseModel:
-    """Class Base Model"""
+    """Represents the BaseModel of the HBnB project."""
 
     def __init__(self, *args, **kwargs):
-        """Constructor"""
-        datenow = datetime.now()
-        if kwargs:
+        """Initialize a new BaseModel.
+
+        Args:
+            *args (any): Unused.
+            **kwargs (dict): Key/value pairs of attributes.
+        """
+        tform = "%Y-%m-%dT%H:%M:%S.%f"
+        self.id = str(uuid4())
+        self.created_at = datetime.today()
+        self.updated_at = datetime.today()
+        if len(kwargs) != 0:
             for k, v in kwargs.items():
-                if k == '__class__':
-                    continue
-                if k in ['created_at', 'updated_at']:
-                    v = datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f")
-                self.__setattr__(k, v)
+                if k == "created_at" or k == "updated_at":
+                    self.__dict__[k] = datetime.strptime(v, tform)
+                else:
+                    self.__dict__[k] = v
         else:
-            self.id = str(uuid4())
-            self.created_at = datenow
-            self.updated_at = datenow
             models.storage.new(self)
 
-    def __str__(self):
-        """String representation"""
-        return "[{}] ({}) {}".format(self.__class__.__name__, self.id,
-                                     self.__dict__)
-
     def save(self):
-        """Updates the updated_at public instance attribute"""
-        self.updated_at = datetime.now()
+        """Update updated_at with the current datetime."""
+        self.updated_at = datetime.today()
         models.storage.save()
 
     def to_dict(self):
-        """Convert object to dictionary representation"""
-        dct = self.__dict__.copy()
-        dct['__class__'] = self.__class__.__name__
-        dct['created_at'] = self.created_at.isoformat()
-        dct['updated_at'] = self.updated_at.isoformat()
-        return dct
+        """Return the dictionary of the BaseModel instance.
+
+        Includes the key/value pair __class__ representing
+        the class name of the object.
+        """
+        rdict = self.__dict__.copy()
+        rdict["created_at"] = self.created_at.isoformat()
+        rdict["updated_at"] = self.updated_at.isoformat()
+        rdict["__class__"] = self.__class__.__name__
+        return rdict
+
+    def __str__(self):
+        """Return the print/str representation of the BaseModel instance."""
+        clname = self.__class__.__name__
+        return "[{}] ({}) {}".format(clname, self.id, self.__dict__)
 
