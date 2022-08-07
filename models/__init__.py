92b02757533bd98b1fca6b0caf345da819626e33
lidu-1 <lilijoli65@gmail.com>
2022-08-07 12:17:56 +0300

    initial commmit

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,8 @@
 #!/usr/bin/python3
-""" init models """
-
+"""__init__ magic method for models directory"""
 from models.engine.file_storage import FileStorage
+
 
 storage = FileStorage()
 storage.reload()
+
